9cacc48e562f53b7a619b378f9659dbb339f2680
PlaiyTiziano <tiziano.plaiy@gmail.com>
2022-12-02 21:44:06 +0100

    Finetuned day 1

--- day1/exercise.rb
+++ day1/exercise.rb
@@ -1,9 +1,11 @@
-totals = [0]
+# frozen_string_literal: true
 
-File.foreach("./input.txt") { |line| line == "\n" ? totals.push(0) : totals[-1] += line.to_i }
+calories = File.open('./input.txt')
+               .readlines
+               .each_with_object([0]) do |line, totals|
+                 line == "\n" ? totals.push(0) : totals[-1] += line.to_i
+               end
+               .sort
 
-puts "part 1 #{puts totals.max}"
-
-totals = totals.sort
-
-puts totals[-1] + totals[-2] + totals[-3]
+puts "part 1 #{calories[-1]}"
+puts "part 2 #{calories.last(3).sum}"
